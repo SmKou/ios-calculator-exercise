9c52479c31ccc3ea099922eeb7f0a8c06d6e1c4d
SmKou <66178856+SmKou@users.noreply.github.com>
2025-07-17 15:28:38 -0700

    Change format of ops fns (may need to change for modular

--- src/main.js
+++ src/main.js
@@ -19,21 +19,18 @@
 
 import './style.css'
 
-const is_op = o => "-+*/=".includes(o)
 const get_e = id => document.getElementById(id)
 const node = (v = "", left = null, right = null) => ({ v, left, right })
 
 const disp = get_e("display")
 const equ = {
-	mem: 0,
-	view: "",
-	tree: {},
-	branch: {}
+	mem: 0,			// last calculated value
+	view: "",		// rep. of equation
+	root: null,	// tree of equation
 }
 
-for (let i = 0; i < 10; ++i) {
-	const btn = document.getElementById(`n${i}-btn`)
-	btn.addEventListener("click", () => {
+for (let i = 0; i < 10; ++i)
+	get_e(`n${i}-btn`).addEventListener("click", () => {
 		const txt = disp.innerHTML
 		if (!Number(txt) && i !== 0)
 			disp.innerHTML = i
@@ -42,28 +39,63 @@ for (let i = 0; i < 10; ++i) {
 		else
 			disp.innerHTML = i
 	})
-}
 
-const ops = {
-	addition: "+",
-	subtraction: "-",
-	multiplication: "*",
-	division: "/"
-}
-for (const key of Object.keys(ops)) {
-	const btn = get_e(`${key}-btn`)
-}
+get_e("addition-btn")
+.addEventListener("click", () => {
+	const n = Number(disp.innerHTML)
+	equ.root = node("+", node(n, equ.root))
+	equ.view += ` + ${n}`
+	disp.innerHTML = ""
+})
 
-const clear = get_e("clear-btn")
-clear.addEventListener("click", () => {
+get_e("subtraction-btn")
+.addEventListener("click", () => {
+	const n = Number(disp.innerHTML)
+	equ.root = node("-", node(n, equ.root))
+	equ.view += ` - ${n}`
+	disp.innerHTML = ""
+})
+
+get_e("multiplication-btn")
+.addEventListener("click", () => {
+	const n = Number(disp.innerHTML)
+	const m = node("*", equ.root.right, n)
+	equ.root.right = m
+	equ.view += ` * ${n}`
+	disp.innerHTML = ""
+})
+
+get_e("division-btn")
+.addEventListener("click", () => {
+	const n = Number(disp.innerHTML)
+	const d = node("/", equ.root.right, n)
+	equ.root.right = d
+	equ.view += ` / ${n}`
+	disp.innerHTML = ""
+})
+
+get_e("clear-btn")
+.addEventListener("click", () => {
 	disp.innerHTML = "0"
 })
 
-const backspace = get_e("backspace-btn")
-backspace.addEventListener("click", () => {
+get_e("backspace-btn")
+.addEventListener("click", () => {
 	const txt = disp.innerHTML
 	if (txt.length)
 		disp.innerHTML = txt.slice(0, txt.length - 1)
 })
 
-const equal = get_e("equal-btn")
+const solve = () => {
+	let curr = equ.root
+	while (curr.left)
+		curr = curr.left
+}
+
+get_e("equal-btn")
+.addEventListener("click", () => {
+	console.log(equ.view)
+	const txt = disp.innerHTML
+	if (txt.length)
+		equ.root.right = node(Number(txt))
+})
