aa8e307d749374147a62f3c893bec6c8c2e90ceb
SmKou <66178856+SmKou@users.noreply.github.com>
2025-07-24 13:32:43 -0700

    BUG show current solution upon operation leads to string concatenation

--- src/main.js
+++ src/main.js
@@ -1,96 +1,107 @@
 import './style.css'
 
-const equ = {
-	mem: null,
-	root: null
-}
-
-/* ------------------------------------------------ HELPERS */
 const get_e = id => document.getElementById(id)
 const display = get_e("display")
 const get_disp = () => display.innerHTML
 const set_disp = (value) => display.innerHTML = value
-const create_node = (value, left = null, right = null) => ({ value, left, right })
-
-/* ------------------------------------------------ FUNCTIONS */
-const num = (idx) => () => {
-	const n = get_disp()
-	set_disp(n ? n + idx : idx)
-	console.log(n, idx)				// testing
-}
-
-const operate = (sym) => {
-	switch (sym) {
-		case "+":
-			return (left, right) => left + right
-		case "-":
-			return (left, right) => left - right
-		case "x":
-			return (left, right) => left * right
-		case "/":
-			return (left, right) => left / right
-	}
-}
+const node = (value, left = null, right = null) => ({ value, left, right })
 
 const operations = {
-	division: {
-		sym: "/",
-		op: operate("/")
-	},
-	multiplication: {
-		sym: "x",
-		op: operate("x")
-	},
-	subtraction: {
-		sym: "-",
-		op: operate("-")
-	},
-	addition: {
-		sym: "+",
-		op: operate("+")
+	division: "/",
+	multiplication: "x",
+	subtraction: "-",
+	addition: "t"
+}
+
+const equ = {
+	mem: null,
+	root: null,
+	add_right: null,
+	last_op: ""
+}
+
+const solve = (curr = equ.root) => {
+	if (!curr)
+		return 0
+	if (!curr.left && !curr.right)
+		return Number(curr.value)
+	switch (curr.value) {
+		case "-":
+			return solve(curr.left) - solve(curr.right)
+		case "t":
+			return solve(curr.left) + solve(curr.right)
+		case "x":
+			return solve(curr.left) * solve(curr.right)
+		case "/":
+			return solve(curr.left) / solve(curr.right)
 	}
 }
 
-const print = (node = equ.root) => {
-	if (!node)
-		return ""
-	if (!node.left && !node.right)
-		return node.value
-	return print(node.left) + " " + print(node.right)
-}
-
-const solve = () => {}
-
-/* ------------------------------------------------ INTERFACE */
 for (let i = 0; i < 10; ++i)
-	get_e(`n${i}-btn`).addEventListener("click", num(i))
+	get_e(`n${i}-btn`).addEventListener("click", () => {
+		const n = get_disp()
+		set_disp(Number(n) ? n + i : i)
+	})
 
 get_e("clear-btn").addEventListener("click", () => {
 	const n = get_disp()
-	set_disp(n ? 0 : "")
-	console.log(n)					// testing
+	if (n)
+		set_disp(0)
+	else {
+		set_disp("")
+		equ.mem = null
+		equ.root = null
+		equ.add_right = null
+		equ.last_op = null
+	}
 })
 
 get_e("backspace-btn").addEventListener("click", () => {
 	const n = get_disp()
 	set_disp(n ? n.slice(0, n.length - 1) : "")
-	console.log(n, n.slice(0, n.length - 1))
 })
 
-
-// Forgot: branch shifting - refer original problem
 for (const key of Object.keys(operations)) {
-	const btn = get_e(key + "-btn")
-	btn.addEventListener("click", () => {
-		if (!equ.root)
-			equ.root = create_node(operations[key].sym, create_node(get_disp()))
-		else {
-			let curr = equ.root
-			while (curr.right)
-				curr = curr.right
-				curr.right = create_node(get_disp())
+	const sym = operations[key]
+	const add_to_tree = ["-", "+"].includes(sym)
+		? () => {
+			if (!equ.root)
+				equ.root = node(sym, equ.mem ? equ.mem : get_disp())
+			else {
+				if (["/", "x"].includes(equ.last_op))
+					equ.add_right = equ.root
+				equ.add_right.right = node(get_disp())
+				equ.root = node(sym, equ.add_right)
+			}
+			equ.add_right = equ.root
+			equ.last_op = sym
+			set_disp(solve())
 		}
-		set_disp("")
-		console.log(key, print())
-	})
+		: () => {
+			if (!equ.root) {
+				equ.root = node(sym, node(get_disp()))
+				equ.add_right = equ.root
+			}
+			else {
+				equ.add_right.right = node(sym, node(get_disp()))
+				equ.add_right = equ.add_right.right
+			}
+			equ.last_op = sym
+			set_disp(solve())
+		}
+	get_e(key + "-btn").addEventListener("click", add_to_tree)
 }
+
+get_e("equal-btn").addEventListener("click", () => {
+	if (!equ.root)
+		return;
+	const n = get_disp()
+	equ.add_right.right = node(n)
+
+	const sol = solve()
+	set_disp(sol)
+	equ.mem = sol
+	equ.root = node(sol)
+	equ.add_right = null
+	equ.last_op = ""
+})
